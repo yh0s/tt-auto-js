4cd74168241dcc5543521022426f60974c5c000c
yhos <263340289+yh0s@users.noreply.github.com>
2026-03-01 21:11:43 +0900

    [change] launcherを新構成に対応

--- tt-auto-launcher.js
+++ tt-auto-launcher.js
@@ -1,6 +1,8 @@
 (function () {
     var rawUrl = 'https://cdn.jsdelivr.net/gh/yh0s/tt-auto-js@main/tt-auto.js' + '?t=' + Date.now();
     var s = document.createElement('script');
+    s.type = 'module';
+
     if (window.trustedTypes && window.trustedTypes.createPolicy) {
         var policyName = 'tt-loader-' + Date.now();
         try {
@@ -10,5 +12,6 @@
             s.src = policy.createScriptURL(rawUrl);
         } catch (e) { s.src = rawUrl; }
     } else { s.src = rawUrl; }
+
     document.head.appendChild(s);
 })();
